0870dea8f876ac1b9fd5bcaa0f34d71b780545ed
ywy <a2433981298@163.com>
2020-06-10 15:24:09 +0800

    Home-Header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class='header'>
+        <div class='header-left'>
+            <i class='iconfont icondenglu'></i>
+        </div>
+        <div class='header-middle'>
+            <i class='iconfont iconsousuo'></i>
+            输入城市/景点/游玩主题
+        </div>
+        <div class='header-right'>
+            北京
+            <i class='iconfont iconjiantou'></i>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+
+</script>
+
+<style lang='stylus' scoped>
+// 1rem = html font-size = 100px
+    @import '~styles/varibles.styl';
+    .header
+        height .45rem
+        background $bgColor
+        display flex
+        color #fff
+        .header-left
+            width 0.4rem
+            text-align center
+            line-height .45rem
+            .icondenglu
+                font-size .3rem
+        .header-middle
+            width 3.1rem
+            height 0.2rem
+            background #fff
+            border-radius 5px
+            margin 0.08rem 0 0.08rem
+            padding 0.07rem 0.1rem 0.03rem
+            font-size 0.15rem
+            color #E8E9EB
+        .header-right
+            width 0.5rem
+            font-size 0.15rem
+            position relative
+            line-height .45rem
+            margin-left 0.05rem
+            overflow hidden
+            text-overflow ellipsis
+            white-space nowrap;
+            .iconjiantou
+                position absolute
+                right 0
+
+</style>
