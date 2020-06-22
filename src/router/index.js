4eb114582edc37d44cf515fb10f355c7a03d2144
ywy <a2433981298@163.com>
2020-06-22 23:21:29 +0800

    添加动态路由

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,14 @@ export default new Router({
       path: '/city',
       name: 'City',
       component: City
+    },
+    {
+      path: '/detail/:id',
+      name: 'Detail',
+      component: () => import('@/pages/detail/Detail')
     }
-  ]
+  ],
+  scrollBehavior (to, from, savedPosition) { // 意思是每次进入初始化页面，X轴为0 Y轴也为0
+    return {x: 0, y: 0}
+  }
 })
